6bac3cbc667094a148172a2f27dce17067d7b0e6
Muhammed Hussein karimi <m.h.karimi1383@outlook.com>
2019-03-10 04:00:23 -0600

    Run this for setup in Debian Based OSes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 echo 'Setting up digikala_history'
 git clone https://github.com/jadijadi/digikala_history/
 cd digikala_history
+sudo pip3 install -r requirements.txt
 sudo apt install python-stdeb
 python setup.py --command-packages=stdeb.command bdist_deb
 echo 'Setup finished'
