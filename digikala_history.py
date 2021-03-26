e09e9cd091b1127ffa129f709b42cbd4db2d47a6
pickle-rick <13noori79@gmail.com>
2021-03-26 15:24:12 +0430

    fix typo error

--- digikala_history.py
+++ digikala_history.py
@@ -249,7 +249,7 @@ class ProcessThread(QThread):
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        # PyInstaller creates a temp folder and stores path in _MEIPASS.
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.dirname(os.path.abspath(__file__))
